2e33328f735fc9d22a47d2acf360a771bf6a7e3c
Hajrus <michalklis@email.cz>
2020-04-19 19:22:36 +0200

    To same jako predtim

--- src/operator.h
+++ src/operator.h
@@ -6,42 +6,42 @@
 namespace IVS{
     class Plus : public Operator{
         using Operator::Operator;
-        private:
+        public:
             void execute() override; 
     };
     class Minus : public Operator{
         using Operator::Operator;
-        private:
+        public:
             void execute() override; 
     };
     class Multiply : public Operator{
         using Operator::Operator;
-        private:
+        public:
             void execute() override; 
     };
     class Divide : public Operator{
         using Operator::Operator;
-        private:
+        public:
             void execute() override; 
     };
     class Power : public Operator{
         using Operator::Operator;
-        private:
+        public:
             void execute() override; 
     };
     class Sqrt : public Operator{
         using Operator::Operator;
-        private:
+        public:
             void execute() override; 
     };
     class Mod : public Operator{
         using Operator::Operator;
-        private:
+        public:
             void execute() override; 
     };
     class Factorial : public Operator{
         using Operator::Operator;
-        private:
+        public:
             void execute() override; 
     };
 };
