d40faad2ac7fd29a8a05a75c95fff08d18c7464a
Hajrus <michalklis@email.cz>
2020-04-20 11:14:37 +0200

    Změna u Math lib na Namespace + implementace knihovny do kaklukacky

--- src/lib/Math.h
+++ src/lib/Math.h
@@ -24,88 +24,87 @@
  * @author Široký Ondřej (xsirok09)
  * @author Sladký Jan (xsladk09)
  * 
- * @brief Math class header file - math library
+ * @brief Math header file - math library
  */
 
 
 /**
- * Math class, implementation of mathematical library for Calculator 
+ * Math, implementation of mathematical library for Calculator 
  * with basic mathematical operations
  * 
- * @brief Math class - Calculator with basic mathematical operations
+ * @brief Math - Calculator with basic mathematical operations
  */  
-class Math
+namespace Math
 {
-    public:
-        /**
-         * @brief Add
-         * Addition
-         * @param firstOperand First operand of addition
-         * @param secondOperand Second operand of addition
-         * @return returns the result of the addition
-         */
-        double Add(double firstOperand, double secondOperand);
+    /**
+     * @brief Add
+     * Addition
+     * @param firstOperand First operand of addition
+     * @param secondOperand Second operand of addition
+     * @return returns the result of the addition
+     */
+    double Add(double firstOperand, double secondOperand);
 
-        /**
-         * @brief Sub
-         * Subtraction
-         * @param firstOperand First operand of subtraction
-         * @param secondOperand Second operand of subtraction
-         * @return returns the result of the subtraction
-         */
-        double Sub(double firstOperand, double secondOperand);
-        
-        /**
-         * @brief Mult
-         * Multiplication
-         * @param firstOperand First operand of multiplication
-         * @param secondOperand Second operand of multiplication
-         * @return returns the result of the multiplication
-         */
-        double Mult(double firstOperand, double secondOperand);
-        
-        /**
-         * @brief Div
-         * Division
-         * @param numer Value of the quotient numerator
-         * @param denom Value of the quotient denominator
-         * @return returns the result of the division
-         */
-        double Div(double numer, double denom);
+    /**
+     * @brief Sub
+     * Subtraction
+     * @param firstOperand First operand of subtraction
+     * @param secondOperand Second operand of subtraction
+     * @return returns the result of the subtraction
+     */
+    double Sub(double firstOperand, double secondOperand);
+    
+    /**
+     * @brief Mult
+     * Multiplication
+     * @param firstOperand First operand of multiplication
+     * @param secondOperand Second operand of multiplication
+     * @return returns the result of the multiplication
+     */
+    double Mult(double firstOperand, double secondOperand);
+    
+    /**
+     * @brief Div
+     * Division
+     * @param numer Value of the quotient numerator
+     * @param denom Value of the quotient denominator
+     * @return returns the result of the division
+     */
+    double Div(double numer, double denom);
 
-        /**
-         * @brief Factorial
-         * @param n nonnegative integer
-         * @return returns the result of the factorial
-         */
-        unsigned long long Factorial(unsigned int n);
+    /**
+     * @brief Factorial
+     * @param n nonnegative integer
+     * @return returns the result of the factorial
+     */
+    unsigned long long Factorial(unsigned int n);
 
-        /**
-         * @brief Power
-         * Power - For b^a -  a-th power of b.
-         * @param base Base of a power
-         * @param exponent Exponent of a power
-         * @return returns the result of a power
-         */
-        double Power(double base, unsigned int exponent);
+    /**
+     * @brief Power
+     * Power - For b^a -  a-th power of b.
+     * @param base Base of a power
+     * @param exponent Exponent of a power
+     * @return returns the result of a power
+     */
+    double Power(double base, unsigned int exponent);
 
-        /**
-         * @brief GeneralRoot
-         * General root - n√x -  n-th root of a number x.
-         * @param x n√x -  n-th root of a number x
-         * @param n n√x -  n-th root of a number x
-         * @return returns the result of  General root
-         */
-        double GeneralRoot(double x, double n);
-        
-        /**
-         * @brief Mod
-         * Modulo operation
-         * @param numer Value of the quotient numerator
-         * @param denom Value of the quotient denominator.
-         * @return returns the remainder of dividing the arguments
-         */
-        int Mod(int numer, int denom);
+    /**
+     * @brief GeneralRoot
+     * General root - n√x -  n-th root of a number x.
+     * @param x n√x -  n-th root of a number x
+     * @param n n√x -  n-th root of a number x
+     * @return returns the result of  General root
+     */
+    double GeneralRoot(double x, double n);
+    
+    /**
+     * @brief Mod
+     * Modulo operation
+     * @param numer Value of the quotient numerator
+     * @param denom Value of the quotient denominator.
+     * @return returns the remainder of dividing the arguments
+     */
+    int Mod(int numer, int denom);
 };
 
 #endif /* MATH_LIB_H_ */
--- src/operator.cpp
+++ src/operator.cpp
@@ -1,33 +1,30 @@
 #include <math.h>
 #include "operator.h"
+#include "Math.h"
 
 using namespace IVS;
 
 Operand* Plus::execute(){
-    return this->_result = new Operand(this->_o1->ToDouble() + this->_o2->ToDouble());
+    return this->_result = new Operand(Math::Add(this->_o1->ToDouble(), this->_o2->ToDouble()));
 }
 Operand* Minus::execute(){
-    return this->_result = new Operand(this->_o1->ToDouble() - this->_o2->ToDouble());
+    return this->_result = new Operand(Math::Sub(this->_o1->ToDouble(),this->_o2->ToDouble()));
 }
 Operand* Multiply::execute(){
-    return this->_result = new Operand(this->_o1->ToDouble() * this->_o2->ToDouble());
+    return this->_result = new Operand(Math::Mult(this->_o1->ToDouble(), this->_o2->ToDouble()));
 }
 Operand* Divide::execute(){
-    return this->_result = new Operand(this->_o1->ToDouble() / this->_o2->ToDouble());
+    return this->_result = new Operand(Math::Div(this->_o1->ToDouble(), this->_o2->ToDouble()));
 }
 Operand* Power::execute(){
-    return this->_result = new Operand(pow(this->_o1->ToDouble(), this->_o2->ToDouble()));
+    return this->_result = new Operand(Math::Power(this->_o1->ToDouble(), this->_o2->ToDouble()));
 }
 Operand* Sqrt::execute(){
-    return this->_result = new Operand(sqrt(this->_o1->ToDouble()));
+    return this->_result = new Operand(Math::GeneralRoot(this->_o1->ToDouble(), this->_o2->ToDouble()));
 }
 Operand* Mod::execute(){
-    return this->_result = new Operand((int)this->_o1->ToDouble() % (int)this->_o2->ToDouble());
+    return this->_result = new Operand(Math::Mod((int)this->_o1->ToDouble(), (int)this->_o2->ToDouble()));
 }
 Operand* Factorial::execute(){
-    double retval = this->_o1->ToDouble();
-    for(int i = this->_o1->ToDouble(); i >= 1; i--){
-        retval*=i;
-    }
-    return this->_result = new Operand(retval);
+    return this->_result = new Operand(Math::Factorial((unsigned long)(unsigned int)(int)this->_o1->ToDouble()));
 }
